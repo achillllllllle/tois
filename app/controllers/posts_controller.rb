0d558ff3fd8deb20caaee3e01d02e622898cea5b
Constance <constance.lesaffre@hotmail.fr>
2024-03-05 16:38:31 +0100

    update methods in posts controller and a bit of the view new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,19 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @toi = @post.tois.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = Post.find_or_initialize_by(post_params)
+    if @post.new_record?
+      @toi = Toi.create(toi_params)
+      post.tois << @toi
+    else
+      @toi = Toi.find_or_create_by(toi_params)
+      post.tois << @toi unless @post.tois.include?(@toi)
+    end
+
     if @post.save
       redirect_to post_path(@post)
     else
@@ -24,5 +33,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:photo, :review, :rating, :user_id, :toi_id)
+  end
+
+  def toi_params
+    params.require(:toi).permit(:title, :category_id, :location, :description, :trailer)
   end
 end
