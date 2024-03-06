f30daacc68ac04a2e036338b4bb019c5646def50
sachagigoi <156658359+sachagigoi@users.noreply.github.com>
2024-03-06 11:46:17 +0100

    feed-index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,17 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
   end
 
   def my_feed
+    @user = current_user
+    # Friend.create(follower: current_user, following: User.find(2))
+    # Friend.create(follower: current_user, following: User.find(3))
+    # Friend.create(follower: current_user, following: User.find(9))
+    # Friend.create(follower: current_user, following: User.find(7))
+
+    @friends = Friend.where(follower_id: @user.id).pluck(:following_id).uniq!
+    @posts = Post.where(user_id: @friends).order(created_at: :desc)
+    @tois = @posts.map { |post| post.toi }.uniq
   end
 end
