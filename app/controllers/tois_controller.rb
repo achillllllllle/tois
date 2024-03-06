f66c1d1a17a38cd9a33aed4b6c7a547d420c4363
sachagigoi <156658359+sachagigoi@users.noreply.github.com>
2024-03-06 12:40:24 +0100

    feed controller et show tois

--- app/controllers/tois_controller.rb
+++ app/controllers/tois_controller.rb
@@ -1,41 +1,22 @@
 class ToisController < ApplicationController
+  before_action :set_toi, only: [:show]
+
   def index
     @tois = Toi.all
   end
 
-#   def show
-#     @toi = Toi.find(params[:id])
-#   end
+  def show
+    user_friends = Friend.where(follower_id: current_user.id).pluck(:following_id).uniq!
+    @filtered_posts = Post.where(toi_id: @toi.id, user_id: user_friends).order(created_at: :desc)
+  end
 
-#   def new
-#     @toi = Toi.new
-#     @posy = @toi.posts.build
-#   end
+  private
 
-#   def create
-#     @toi = Toi.find_or_initialize_by(toi_params)
-#     if @toi.new_record?
-#       @post = Post.create(post_params)
-#       toi.posts << @post
-#     else
-#       @post = Post.find_or_create_by(post_params)
-#       toi.posts << @post unless @toi.posts.include?(@post)
-#     end
+  def set_toi
+    @toi = Toi.find(params[:id])
+  end
 
-#     if @toi.save
-#       redirect_to toi_path(@toi)
-#     else
-#       render :new, status: :unprocessable_entity
-#     end
-#   end
-
-#   private
-
-#   def toi_params
-#     params.require(:toi).permit(:title, :category_id, :location, :description, :trailer)
-#   end
-
-#   def post_params
-#     params.require(:post).permit(:photo, :review, :rating, :user_id, :toi_id)
-#   end
+  def toi_params
+    params.require(:toi).permit(:title, :category_id, :location, :description, :trailer)
+  end
 end
