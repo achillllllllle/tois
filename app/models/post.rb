e29e94eadfe5edf7868d930abc7bdb349026f765
Oscar Fonty <oscar77.fonty@gmail.com>
2024-03-07 16:45:27 +0100

    stars

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,17 @@ class Post < ApplicationRecord
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 
   accepts_nested_attributes_for :toi
+
+  def starify(rating)
+    stars = ''
+    full_stars = rating / 2
+    half_star = rating % 2
+    half_star.to_i.times do
+      stars += "<i class='fa-solid fa-star-half-stroke fa-flip-horizontal'></i>"
+    end
+    full_stars.to_i.times do
+      stars += "<i class='fa-solid fa-star'></i>"
+    end
+    stars
+  end
 end
