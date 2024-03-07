bc41e4beabea593dff749d033aad3546c3c936e8
Constance <constance.lesaffre@hotmail.fr>
2024-03-07 15:44:42 +0100

    last last last update

--- app/javascript/controllers/post_form_controller.js
+++ app/javascript/controllers/post_form_controller.js
@@ -20,38 +20,11 @@ export default class extends Controller {
 
   createEvent(toiName) {
     this.newToiFormTarget.classList.remove('d-none')
-
-    // fetch(`/api/toi/${toiName}`)
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     // Assuming data contains the fields to prefill
-    //     // You can adapt this part according to your API response
-    //     this.newToiFormTarget.querySelector('[name="field_name"]').value = data.field_name;
-    //     this.newToiFormTarget.querySelector('[name="field_description"]').value = data.field_description;
-    //     // Pre-fill other fields as needed
-    //   })
-
-    // if (toi.category === "cinema") {
-    //   this.trailerInputTarget.classList.remove('d-none');
-    // } else {
-    //   this.trailerInputTarget.classList.add('d-none');
-    // }
-
-    // if (toi.category === "spectacle") {
-    //   this.locationInputTarget.classList.remove('d-none');
-    // } else {
-    //   this.locationInputTarget.classList.add('d-none');
-    // }
-
     return { value:toiName, text:toiName }
   }
 
   itemSelected(value) {
     // console.log('selected', value);
-
-    // if (item.classList.contains("create")) {
-    //   this.newToiFormTarget.classList.add("d-none");
-    // }
   }
 
   showInputs(event) {
