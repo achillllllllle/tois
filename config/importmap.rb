e314fa609a95f1f3c20c5cef14b79d0e49e67709
sachagigoi <156658359+sachagigoi@users.noreply.github.com>
2024-03-12 09:06:41 +0100

    byebye swup

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,6 +9,3 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.3.1/dist/js/tom-select.complete.js" # @2.3.1
 pin "star-rating.js" # @4.3.0
-pin "swup" # @4.6.0
-pin "delegate-it" # @6.0.1
-pin "path-to-regexp" # @6.2.1
